9ae6996bc7eb6e8c3baf8606fbc83ce6d876babb
JonathanB <116760545+JonathanBetPer@users.noreply.github.com>
2024-04-18 16:26:17 +0100

    proyecto inicializado
    Proyecto inicializado y Swagger incorporado

---
+++ src/main/java/me/scholagate/api/configs/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package me.scholagate.api.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Tienda",
+                version = "1.0",
+                description = "API para la gestión de productos de una tienda",
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "Jonathan Betancor Perdomo",
+                        email = "jonathanbetancorperdomo@gmail.com"
+                )
+        )
+)
+public class OpenApiConfig { }
+/*
+
+@Tag(name = "Cliente Controller", description = "Controlador de clientes")
+
+ @Operation(summary = "Obtener todos los clientes")
+
+*/
